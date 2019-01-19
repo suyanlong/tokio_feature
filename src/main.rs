803fa12048f81fb78d58055edd8e102b708a3817
suyanlong <592938410@qq.com>
2019-01-19 22:43:31 +0800

    notify future in tokio reactor.!!!!!

--- src/main.rs
+++ src/main.rs
@@ -3,80 +3,78 @@ extern crate futures;
 extern crate tokio_core;
 
 use std::error::Error;
+use std::thread::{sleep, spawn};
 
 use chrono::prelude::*;
 use chrono::*;
-use futures::done;
-use futures::future::join_all;
-use futures::future::{err, ok};
-use futures::prelude::*;
 use futures::prelude::*;
 use futures::*;
 use tokio_core::reactor::Core;
 
-#[derive(Debug)]
-struct WaitForIt {
-    message: String,
-    until: DateTime<Utc>,
-    polls: u64,
+pub struct WaitInAnotherThread {
+    end_time: DateTime<Utc>,
+    running: bool,
+    count: u64,
 }
 
-impl WaitForIt {
-    pub fn new(message: String, delay: Duration) -> WaitForIt {
-        WaitForIt {
-            polls: 0,
-            message: message,
-            until: Utc::now() + delay,
+impl WaitInAnotherThread {
+    pub fn new(how_long: Duration) -> WaitInAnotherThread {
+        WaitInAnotherThread {
+            end_time: Utc::now() + how_long,
+            running: false,
+            count: 0,
         }
+    }
+
+    fn run(&mut self, task: task::Task) {
+        let lend = self.end_time;
+
+        spawn(move || {
+            while Utc::now() < lend {
+                let delta_sec = lend.timestamp() - Utc::now().timestamp();
+                if delta_sec > 0 {
+                    sleep(::std::time::Duration::from_secs(delta_sec as u64));
+                }
+                // 通知，完成了这个任务！！！
+                task.notify();
+            }
+            println!("the time has come == {:?}!", lend);
+        });
     }
 }
 
-impl Future for WaitForIt {
-    type Item = String;
+impl Future for WaitInAnotherThread {
+    type Item = ();
     type Error = Box<Error>;
 
     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
-        let now = Utc::now();
-        if self.until < now {
-            Ok(Async::Ready(format!(
-                "{} after {} polls!",
-                self.message, self.polls
-            )))
-        } else {
-            self.polls += 1;
+        self.count += 1;
+        println!("count = {}", self.count);
+        if Utc::now() < self.end_time {
+            println!("not ready yet! parking the task.");
 
-            println!("not ready yet --> {:?}", self);
-            // 只有调用这个方法吗，在poll内部，难道没有其他的办法吗？一直调用poll方法啊！！！！。
-            // 一秒钟被调用35474次poll方法，就是一直循环的调用，如果注释这句，却只能背调用一次，难道没有通知机制？？
-            // 频繁的调用，多占用CPU！浪费资源。poll，不亏叫这个名字，就只一直循环的poll。
-            futures::task::current().notify();
+            if !self.running {
+                println!("side thread not running! starting now!");
+                // 终于找到了，这个方法了。这个就是通知的方法呀。
+                // 当前的只有自己，因为reactor是单线程的，正执行到这个方法内部，肯定是自己的token id了！！！！
+                self.run(task::current());
+                self.running = true;
+            }
+
             Ok(Async::NotReady)
+        } else {
+            println!("ready! the task will complete.");
+            Ok(Async::Ready(()))
         }
     }
 }
 
+// https://zhuanlan.zhihu.com/p/51784496 重要！！！！
 fn main() {
     let mut reactor = Core::new().unwrap();
 
-    let wfi_1 = WaitForIt::new("I'm done:".to_owned(), Duration::seconds(1));
-    println!("wfi_1 == {:?}", wfi_1);
-    let wfi_2 = WaitForIt::new("I'm done too:".to_owned(), Duration::seconds(2));
-    println!("wfi_2 == {:?}", wfi_2);
-
-    let v = vec![wfi_1, wfi_2];
-
-    //let sel = join_all(v);
-    let sel = select_ok(v);
-
-    let ret = reactor.run(sel).unwrap();
-    println!("ret == {:?}", ret);
+    let wiat = WaitInAnotherThread::new(Duration::seconds(3));
+    println!("wait future started");
+    let ret = reactor.run(wiat).unwrap();
+    println!("wait future completed. ret == {:?}", ret);
 }
-//....
-//not ready yet --> WaitForIt { message: "I\'m done:", until: 2019-01-19T14:12:10.691746Z, polls: 35468 }
-//not ready yet --> WaitForIt { message: "I\'m done:", until: 2019-01-19T14:12:10.691746Z, polls: 35469 }
-//not ready yet --> WaitForIt { message: "I\'m done:", until: 2019-01-19T14:12:10.691746Z, polls: 35470 }
-//not ready yet --> WaitForIt { message: "I\'m done:", until: 2019-01-19T14:12:10.691746Z, polls: 35471 }
-//not ready yet --> WaitForIt { message: "I\'m done:", until: 2019-01-19T14:12:10.691746Z, polls: 35472 }
-//not ready yet --> WaitForIt { message: "I\'m done:", until: 2019-01-19T14:12:10.691746Z, polls: 35473 }
-//not ready yet --> WaitForIt { message: "I\'m done:", until: 2019-01-19T14:12:10.691746Z, polls: 35474 }
-//ret == ["I\'m done: after 35474 polls!"]
