d576c44b9771674f59fc5acdd31d563aff8e5c5e
suyanlong <592938410@qq.com>
2019-01-19 22:25:18 +0800

    tokio reactor implement Future

--- src/main.rs
+++ src/main.rs
@@ -1,158 +1,82 @@
+extern crate chrono;
 extern crate futures;
-extern crate futures_cpupool;
-extern crate httparse;
-extern crate hyper;
-extern crate parking_lot;
-extern crate rand;
-extern crate serde;
-#[macro_use]
-extern crate serde_derive;
-extern crate serde_json;
-extern crate threadpool;
-extern crate tokio_service;
+extern crate tokio_core;
 
-use parking_lot::Mutex;
-use std::io;
-use futures::{BoxFuture, Future};
-use futures_cpupool::CpuPool;
-use rand::Rng;
-use std::sync::Arc;
-use std::collections::HashMap;
-use futures::sync::oneshot;
-use std::collections::VecDeque;
+use std::error::Error;
 
-use futures::future::FutureResult;
-use hyper::{Get, Post, StatusCode};
-use hyper::header::ContentLength;
-use hyper::server::{Http, NewService, Request, Response, Service};
+use chrono::prelude::*;
+use chrono::*;
+use futures::done;
+use futures::future::join_all;
+use futures::future::{err, ok};
+use futures::prelude::*;
+use futures::prelude::*;
+use futures::*;
+use tokio_core::reactor::Core;
 
-use std::thread;
-use std::time::Duration;
-use std::ops::Deref;
-use std::ops::DerefMut;
+#[derive(Debug)]
+struct WaitForIt {
+    message: String,
+    until: DateTime<Utc>,
+    polls: u64,
+}
 
-impl NewService for Server {
-    type Request = Request;
-    type Response = Response;
-    type Error = hyper::Error;
-    type Instance = Server;
-    fn new_service(&self) -> io::Result<Server> {
-        Ok(Server {
-            hash_map: self.hash_map.clone(),
-        })
+impl WaitForIt {
+    pub fn new(message: String, delay: Duration) -> WaitForIt {
+        WaitForIt {
+            polls: 0,
+            message: message,
+            until: Utc::now() + delay,
+        }
     }
 }
 
-#[derive(Serialize)]
-struct Message {
-    id: i32,
-    body: String,
-}
+impl Future for WaitForIt {
+    type Item = String;
+    type Error = Box<Error>;
 
-struct Server {
-    hash_map: Arc<Mutex<VecDeque<(String, oneshot::Sender<String>)>>>,
-}
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        let now = Utc::now();
+        if self.until < now {
+            Ok(Async::Ready(format!(
+                "{} after {} polls!",
+                self.message, self.polls
+            )))
+        } else {
+            self.polls += 1;
 
-use futures::Stream;
-
-impl Service for Server {
-    type Request = Request;
-    type Response = Response;
-    type Error = hyper::Error;
-    type Future = BoxFuture<Response, hyper::Error>;
-
-    fn call(&self, req: Request) -> Self::Future {
-        println!("method: {:?}, path: {:?}, ", req.method(), req.path());
-        let (tx, rx) = oneshot::channel();
-        //println!("-----post --{:?}----", req.slice((1, 100)));
-        let random_id = rand::thread_rng().gen_range(1, 50000);
-        println!("-----call -----{:?}-", random_id);
-        {
-            self.hash_map.lock().push_back((random_id.to_string(), tx));
+            println!("not ready yet --> {:?}", self);
+            // 只有调用这个方法吗，在poll内部，难道没有其他的办法吗？一直调用poll方法啊！！！！。
+            // 一秒钟被调用35474次poll方法，就是一直循环的调用，如果注释这句，却只能背调用一次，难道没有通知机制？？
+            // 频繁的调用，多占用CPU！浪费资源。poll，不亏叫这个名字，就只一直循环的poll。
+            futures::task::current().notify();
+            Ok(Async::NotReady)
         }
-
-        //这个时候已经可以了啊，满足我们的需求了。
-        rx.map(|item| {
-            println!("=={:?}==", item);
-            let mut res = Response::new();
-            res.set_body(item);
-            res
-        }).map_err(|_| hyper::Error::Timeout)
-            .boxed()
-
-        //        rx.map_err(|_| hyper::Error::from(io::Error::from_raw_os_error(1)))
-        //            .boxed()
-        //                let mut body = req.body();
-        //                loop {
-        //                    let data = body.poll();
-        //                    body.for_each();
-        //                    println!("-----data---{:?}", data);
-        //                }
-        //                req.body().for_each(move |chunk| {
-        //                    println!("-msg--{:?}", chunk);
-        //                        let (tx, rx) = oneshot::channel();
-        //                        let random_id = rand::thread_rng().gen_range(1, 50000);
-        //                        self.thread_pool.lock().execute(move || {
-        //                            println!(
-        //                                "-thread id = {:?}-------k ={:?}",
-        //                                thread::current().id(),
-        //                                random_id
-        //                            );
-        //                            let mut response = Response::new();
-        //                            response.set_body(random_id.to_string());
-        //                            tx.send(response);
-        //                        });
-        //                    Ok(())
-        //                });
-        //                    .map(move |rx| {
-        //                        let mut response = Response::new();
-        //                        //                response.set_body(random_id.to_string());
-        //                        response
-        //                    })
-        //                    .boxed()
-        //                let mut response = Response::new();
-        //                println!("--------");
-        //                        response.set_body(random_id.to_string());
-        //                        response
-        //                futures::finished(response).boxed()
-        //                rx.map_err(|_| hyper::Error::Version).boxed()
-        //                rx.map_err()
-        //                    .map(move |_| {
-        //                        let res = rx.
-        //
-        //
-        //                    })
-        //                    .boxed()
     }
 }
 
 fn main() {
-    let addr = "127.0.0.1:8080".parse().unwrap();
-    let arc_hash_map = Arc::new(Mutex::new(VecDeque::new()));
-    let hash_map = arc_hash_map.clone();
+    let mut reactor = Core::new().unwrap();
 
-    let _ = thread::spawn(move || {
-        while true {
-            thread::sleep(Duration::from_secs(1));
-            let mut map = arc_hash_map.lock();
-            let map: &mut VecDeque<(String, oneshot::Sender<String>)> = map.deref_mut();
-            map.pop_front().map(|(k, v)| {
-                println!("----k = {:?}", k);
-                //                let mut response = Response::new();
-                //                response
-                //                    .headers_mut()
-                //                    .set_raw("Content-Type", "application/json");
-                //                response.set_body(k);
-                v.send(k);
-            });
-        }
-    });
+    let wfi_1 = WaitForIt::new("I'm done:".to_owned(), Duration::seconds(1));
+    println!("wfi_1 == {:?}", wfi_1);
+    let wfi_2 = WaitForIt::new("I'm done too:".to_owned(), Duration::seconds(2));
+    println!("wfi_2 == {:?}", wfi_2);
 
-    let server = Server { hash_map: hash_map };
-    let server = Http::new().bind(&addr, server).unwrap();
-    println!(
-        "Listening on http://{} with 1 thread.",
-        server.local_addr().unwrap()
-    );
-    server.run().unwrap();
+    let v = vec![wfi_1, wfi_2];
+
+    //let sel = join_all(v);
+    let sel = select_ok(v);
+
+    let ret = reactor.run(sel).unwrap();
+    println!("ret == {:?}", ret);
 }
+//....
+//not ready yet --> WaitForIt { message: "I\'m done:", until: 2019-01-19T14:12:10.691746Z, polls: 35468 }
+//not ready yet --> WaitForIt { message: "I\'m done:", until: 2019-01-19T14:12:10.691746Z, polls: 35469 }
+//not ready yet --> WaitForIt { message: "I\'m done:", until: 2019-01-19T14:12:10.691746Z, polls: 35470 }
+//not ready yet --> WaitForIt { message: "I\'m done:", until: 2019-01-19T14:12:10.691746Z, polls: 35471 }
+//not ready yet --> WaitForIt { message: "I\'m done:", until: 2019-01-19T14:12:10.691746Z, polls: 35472 }
+//not ready yet --> WaitForIt { message: "I\'m done:", until: 2019-01-19T14:12:10.691746Z, polls: 35473 }
+//not ready yet --> WaitForIt { message: "I\'m done:", until: 2019-01-19T14:12:10.691746Z, polls: 35474 }
+//ret == ["I\'m done: after 35474 polls!"]
